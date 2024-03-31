2063099a90dbee770d2eaa136956b3fdf4998b46
onesirian <ldlockhart@gmail.com>
2024-03-31 18:28:13 -0500

    Add button, config for google login

--- src/main/java/com/topsoutherncoders/ytgetmeout/HelloWorldView.java
+++ src/main/java/com/topsoutherncoders/ytgetmeout/HelloWorldView.java
@@ -2,7 +2,6 @@ package com.topsoutherncoders.ytgetmeout;
 
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
 @Route("hello")
---
+++ src/main/java/com/topsoutherncoders/ytgetmeout/LoginView.java
@@ -0,0 +1,28 @@
+package com.topsoutherncoders.ytgetmeout;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinServlet;
+import org.apache.juli.logging.Log;
+import org.springframework.beans.factory.annotation.Value;
+
+@Route("login")
+public class LoginView extends VerticalLayout {
+
+    @Value("${oauth2.client-id")
+    private String clientId;
+    public LoginView() {
+        Button loginButton = new Button("Login with Google");
+        loginButton.addClickListener(event -> {
+            //redirect
+            String authorizationUrl = "https://accounts.google.com/o/oauth2/auth"
+                    + "?client_id=" + clientId
+                    + "&redirect_uri=" + VaadinServlet.getCurrent().getServletContext().getContextPath() + "/oauth2callback"
+                    + "&response_type=code"
+                    + "&scope=email%20profile"; // Example scope
+            getUI().ifPresent(ui -> ui.getPage().executeJs("window.location.href='" + authorizationUrl + "'"));
+        });
+        add(loginButton);
+    }
+}
